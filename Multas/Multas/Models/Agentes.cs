9831d7e05fb62f672edc8be06f94255ef9f9f29f
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2019-04-30 16:40:39 +0100

    Adicionar anotadores para formatar o comportamento do modelo Agentes

--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Multas.Models {
    public class Agentes {
 
+      [Key] // identifica este atributo como Primary Key
       public int ID { get; set; }
 
+      [Required (ErrorMessage ="O Nome é de preenchimento obrigatório.") ]
+      [StringLength (50,ErrorMessage ="O {0} deve ter, no máximo, {1} caracteres.")]
+      [RegularExpression("[A-ZÁÉÍÓÚ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+(( | e | de | do | dos | da | das |-|')[A-ZÁÉÍÓÚ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+)*",
+                          ErrorMessage ="O {0} só pode conter letras. Cada palavra deve começar com uma Maiúscula.")]
       public string Nome { get; set; }
 
+      [Required(ErrorMessage = "A Esquadra é de preenchimento obrigatório.")]
+      [StringLength(20, ErrorMessage = "A {0} deve ter, no máximo, {1} caracteres.")]
+      [RegularExpression("[A-Z][a-z]+(( |-)[A-Z][a-z]+)*",
+                          ErrorMessage = "O {0} só pode conter letras. Cada palavra deve começar com uma Maiúscula.")]
       public string Esquadra { get; set; }
+
 
       public string Fotografia { get; set; }
 
