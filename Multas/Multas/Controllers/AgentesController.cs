d3b472dc52dbbb47a288315ee247a7051292525d
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2019-05-14 14:59:34 +0100

    Adição de Try-Catch para evitar exceções  na remoção de agentes

--- Multas/Multas/Controllers/AgentesController.cs
+++ Multas/Multas/Controllers/AgentesController.cs
@@ -8,120 +8,118 @@ using System.Web;
 using System.Web.Mvc;
 using Multas.Models;
 
-namespace Multas.Controllers
-{
-    public class AgentesController : Controller
-    {
-        private MultasDB db = new MultasDB();
+namespace Multas.Controllers {
+   public class AgentesController : Controller {
+      private MultasDB db = new MultasDB();
 
-        // GET: Agentes
-        public ActionResult Index()
-        {
-            return View(db.Agentes.ToList());
-        }
+      // GET: Agentes
+      public ActionResult Index() {
+         return View(db.Agentes.ToList());
+      }
 
-        // GET: Agentes/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Agentes agentes = db.Agentes.Find(id);
-            if (agentes == null)
-            {
-                return HttpNotFound();
-            }
-            return View(agentes);
-        }
+      // GET: Agentes/Details/5
+      public ActionResult Details(int? id) {
+         if(id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Agentes agentes = db.Agentes.Find(id);
+         if(agentes == null) {
+            return HttpNotFound();
+         }
+         return View(agentes);
+      }
 
-        // GET: Agentes/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+      // GET: Agentes/Create
+      public ActionResult Create() {
+         return View();
+      }
 
-        // POST: Agentes/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes , HttpPostedFileBase fotografia)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Agentes.Add(agentes);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(agentes);
-        }
-
-        // GET: Agentes/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Agentes agentes = db.Agentes.Find(id);
-            if (agentes == null)
-            {
-                return HttpNotFound();
-            }
-            return View(agentes);
-        }
-
-        // POST: Agentes/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(agentes).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(agentes);
-        }
-
-        // GET: Agentes/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Agentes agentes = db.Agentes.Find(id);
-            if (agentes == null)
-            {
-                return HttpNotFound();
-            }
-            return View(agentes);
-        }
-
-        // POST: Agentes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Agentes agentes = db.Agentes.Find(id);
-            db.Agentes.Remove(agentes);
+      // POST: Agentes/Create
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Create([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes, HttpPostedFileBase fotografia) {
+         if(ModelState.IsValid) {
+            db.Agentes.Add(agentes);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
+         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+         return View(agentes);
+      }
+
+      // GET: Agentes/Edit/5
+      public ActionResult Edit(int? id) {
+         if(id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Agentes agentes = db.Agentes.Find(id);
+         if(agentes == null) {
+            return HttpNotFound();
+         }
+         return View(agentes);
+      }
+
+      // POST: Agentes/Edit/5
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Edit([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes) {
+         if(ModelState.IsValid) {
+            db.Entry(agentes).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+         }
+         return View(agentes);
+      }
+
+      // GET: Agentes/Delete/5
+      public ActionResult Delete(int? id) {
+         if(id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Agentes agentes = db.Agentes.Find(id);
+         if(agentes == null) {
+            return HttpNotFound();
+         }
+         return View(agentes);
+      }
+
+      // POST: Agentes/Delete/5
+      [HttpPost, ActionName("Delete")]
+      [ValidateAntiForgeryToken]
+      public ActionResult DeleteConfirmed(int id) {
+
+         Agentes agente = db.Agentes.Find(id);
+
+         try {
+            db.Agentes.Remove(agente);
+            db.SaveChanges();
+         }
+         catch(Exception) {
+            // captura a excessão e processa o código para resolver o problema
+            // pode haver mais do que um 'catch' associado a um 'try'
+
+            // enviar mensagem de erro para o utilizador
+            ModelState.AddModelError("","Ocorreu um erro com a eliminação do Agente "
+                                        + agente.Nome+
+                                        ". Provavelmente relacionado com o facto do " +
+                                        "agente ter emitido multas...");
+            // devolver os dados do Agente à View
+            return View(agente);
+         }
+
+         // redireciona o interface para a view INDEX associada ao controller Agentes
+         return RedirectToAction("Index");
+      }
+
+      protected override void Dispose(bool disposing) {
+         if(disposing) {
+            db.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+   }
 }
