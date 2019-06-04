85ea3b0e36fb8d8fdf7957cbea58fff79f7b8027
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2019-06-04 14:59:19 +0100

    conclusão da junção das bases de dados de autenticação e do 'negócio' numa única base de dados.

--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -17,14 +17,14 @@ namespace Multas.Models {
 
       [Required(ErrorMessage = "O Nome é de preenchimento obrigatório.")]
       [StringLength(50, ErrorMessage = "O {0} deve ter, no máximo, {1} caracteres.")]
-      [RegularExpression("[A-ZÁÉÍÓÚ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+(( | e | de | do | dos | da | das |-|')[A-ZÁÉÍÓÚ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+)*",
-                          ErrorMessage = "O {0} só pode conter letras. Cada palavra deve começar com uma Maiúscula.")]
+      //[RegularExpression("[A-ZÁÉÍÓÚ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+(( | e | de | do | dos | da | das |-|')[A-ZÁÉÍÓÚ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+)*",
+      //                    ErrorMessage = "O {0} só pode conter letras. Cada palavra deve começar com uma Maiúscula.")]
       public string Nome { get; set; }
 
       [Required(ErrorMessage = "A Esquadra é de preenchimento obrigatório.")]
       [StringLength(20, ErrorMessage = "A {0} deve ter, no máximo, {1} caracteres.")]
-      [RegularExpression("[A-Z][a-z]+(( |-)[A-Z][a-z]+)*",
-                          ErrorMessage = "O {0} só pode conter letras. Cada palavra deve começar com uma Maiúscula.")]
+      //[RegularExpression("[A-Z][a-z]+(( |-)[A-Z][a-z]+)*",
+      //                    ErrorMessage = "O {0} só pode conter letras. Cada palavra deve começar com uma Maiúscula.")]
       public string Esquadra { get; set; }
 
 
--- Multas/Multas/Models/IdentityModels.cs
+++ Multas/Multas/Models/IdentityModels.cs
@@ -1,37 +1,62 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Multas.Models {
+
    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
    public class ApplicationUser : IdentityUser {
+
       public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager) {
          // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
          var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
          // Add custom user claims here
          return userIdentity;
       }
+
    }
 
-   public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
-      public ApplicationDbContext()
-          : base("MultasDbConnectionString", throwIfV1Schema: false) {
+
+   /// <summary>
+   /// apresenta a criação da base de dados
+   /// - dos utilizadores
+   /// - dos dados do 'negócio'
+   /// </summary>
+   public class MultasDB : IdentityDbContext<ApplicationUser> {
+     
+   // construtor que identifica a localização da BD  
+      public MultasDB()
+             : base("MultasDbConnectionString", throwIfV1Schema: false) {
       }
 
-      static ApplicationDbContext() {
+      static MultasDB() {
          // Set the database intializer which is run once during application start
          // This seeds the database with admin user credentials and admin role
-         Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+         Database.SetInitializer<MultasDB>(new ApplicationDbInitializer());
       }
 
-      public static ApplicationDbContext Create() {
-         return new ApplicationDbContext();
+      public static MultasDB Create() {
+         return new MultasDB();
       }
 
       // adicionar aqui as tabelas da BD
+      // definir as tabelas da BD
+      public DbSet<Condutores> Condutores { get; set; }
+      public DbSet<Viaturas> Viaturas { get; set; }
+      public DbSet<Agentes> Agentes { get; set; }
+      public DbSet<Multas> Multas { get; set; }
 
+
+      // método a ser executado no início da criação do Modelo
+      protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+         // eliminar a convenção de atribuir automaticamente o 'on Delete Cascade' nas FKs
+         modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+         modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+         base.OnModelCreating(modelBuilder);
+      }
 
 
    }
